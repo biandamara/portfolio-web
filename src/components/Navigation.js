8a48deac2d371ee285d976c2505e074c57f65e31
Bian Damara <bian.damara@gmail.com>
2022-09-16 02:35:35 +0700

    edit navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,57 @@
 // import styles
-import styles from "../assets/css/Navigation.module.css";
+// import styles from "../assets/css/Navigation.module.css";
 
 function Navigation() {
   return (
     <>
       {/* navigation bar start */}
-      <div className={styles.container}>
-        {/* brand start */}
-        <a href="/">
-          <h6>Logo</h6>
-        </a>
-        {/* brand end */}
+      <nav className="navbar navbar-expand-sm sticky-top bg-light">
+        <div className="container-fluid">
+          {/* brand start */}
+          <a className="navbar-brand" href="/">
+            B D
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          {/* brand end */}
 
-        {/* navigation menus start*/}
-        <a href="/">Home</a>
-        <a href="/about">About Me</a>
-        <a href="/gallery">Mini Gallery</a>
-        <a href="/contact">Contact</a>
-        {/* navigation menus end */}
-      </div>
+          {/* navigation menus start*/}
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="nav">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a className="nav-link" aria-current="page" href="/about">
+                  About
+                </a>
+              </li>
+              <li>
+                <a className="nav-link" aria-current="page" href="/gallery">
+                  Gallery
+                </a>
+              </li>
+              <li>
+                <a className="nav-link" aria-current="page" href="/contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/* navigation menus end */}
+        </div>
+      </nav>
       {/* navigation bar end */}
     </>
   );
