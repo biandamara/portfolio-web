b70590c6026f89b6f3298bc0c5f4d3648e13f15f
Bian Damara <bian.damara@gmail.com>
2022-09-17 13:27:26 +0700

    under maintenance

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,46 @@ import styles from "../assets/css/Footer.module.css";
 function Footer() {
   return (
     <>
-      <div className={styles.container}></div>
+      {/* footer start */}
+      <div className={`fixed-bottom ${styles.footer}`}>
+        <div className="row text-center">
+          <div className="col">
+            {/* instagram start */}
+            <a className="px-5" href="https://www.instagram.com/biandamara/">
+              <img
+                src="https://res.cloudinary.com/biandamara/image/upload/v1663365224/Portfolio%20Web/Icons/instagram_npe7cg.png"
+                alt="Instagram"
+                aria-hidden
+              ></img>
+              {/* instagram end */}
+            </a>
+
+            {/* linkedin start */}
+            <a
+              className="px-5"
+              href="https://www.linkedin.com/in/bian-damara-218270151/"
+            >
+              <img
+                src="https://res.cloudinary.com/biandamara/image/upload/v1663365224/Portfolio%20Web/Icons/linkedin_cyfjuo.png"
+                alt="LinkedIn"
+                aria-hidden
+              ></img>
+            </a>
+            {/* linkedin end */}
+
+            {/* github start */}
+            <a className="px-5" href="https://github.com/biandamara">
+              <img
+                src="https://res.cloudinary.com/biandamara/image/upload/v1663365224/Portfolio%20Web/Icons/github_hzsub0.png"
+                alt="GitHub"
+                aria-hidden
+              ></img>
+            </a>
+            {/* github end */}
+          </div>
+        </div>
+      </div>
+      {/* footer end */}
     </>
   );
 }
--- src/pages/Maintenance.js
+++ src/pages/Maintenance.js
@@ -13,10 +13,22 @@ function Maintenance() {
       {/* component - navigation end */}
 
       {/* page - under maintenance start */}
-      <div className={styles.screen}>
-        <div className={styles.bacrground}>
-          <h1>Coming Soon</h1>
+      <div
+        className={styles.background}
+        style={{
+          // backgroundImage: `url("https://res.cloudinary.com/biandamara/image/upload/v1663393608/Portfolio%20Web/Background/background_egbvyb.jpg")`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        {/* text start */}
+        <div className={styles.box}>
+          <p>{"//"} coming soon</p>
         </div>
+        {/* text end */}
       </div>
       {/* page - under maintenance end */}
 
